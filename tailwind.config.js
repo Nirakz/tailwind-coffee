5289850270d878d2335a1a64cfc6e793cce2fd5d
nirakz <ntchinh271099@gmail.com>
2022-05-11 18:42:32 +0700

    Featured Mugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,14 +17,18 @@ module.exports = {
         slideDown: {
           '0%': { transform: 'translateY(-100%)' },
           '100%': { transform: 'translateY(0)' },
+        },
+        fadeIn: {
+          from: { opacity: 0},
+          to: { opacity: 1 }
         }
       },
       animation: {
         slideDown: 'slideDown .5s ease-in-out',
+        fadeIn: 'fadeIn .5s ease-in-out'
       },
       backgroundImage: {
         'slider-bg': "url('./img/slider-bg.jpeg')",
-        'footer-texture': "url('/img/footer-texture.png')",
       }
     },
   },
